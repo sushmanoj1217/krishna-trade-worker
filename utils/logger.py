2f10511ab7da341ca2e58fae40f76a230b97f791
sushmanoj1217 <pandeymanoj82@gmail.com>
2025-08-19 19:17:57 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,34 +1,31 @@
 import logging, os, sys, time, hashlib
 
-
 _LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 
-
 class DuplicateGuard(logging.Filter):
-def __init__(self, window=5):
-super().__init__()
-self.window = window
-self._seen = {}
-def filter(self, record: logging.LogRecord) -> bool:
-msg = f"{record.levelname}:{record.getMessage()}"
-h = hashlib.md5(msg.encode()).hexdigest()
-now = time.time()
-last = self._seen.get(h, 0)
-self._seen[h] = now
-return (now - last) > self.window
+    def __init__(self, window=5):
+        super().__init__()
+        self.window = window
+        self._seen = {}
 
+    def filter(self, record: logging.LogRecord) -> bool:
+        msg = f"{record.levelname}:{record.getMessage()}"
+        h = hashlib.md5(msg.encode()).hexdigest()
+        now = time.time()
+        last = self._seen.get(h, 0)
+        self._seen[h] = now
+        return (now - last) > self.window
 
 def get_logger(name="krishna"):
-logger = logging.getLogger(name)
-if logger.handlers:
-return logger
-logger.setLevel(_LEVEL)
-h = logging.StreamHandler(sys.stdout)
-fmt = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
-h.setFormatter(fmt)
-h.addFilter(DuplicateGuard())
-logger.addHandler(h)
-return logger
-
+    logger = logging.getLogger(name)
+    if logger.handlers:
+        return logger
+    logger.setLevel(_LEVEL)
+    h = logging.StreamHandler(sys.stdout)
+    fmt = logging.Formatter("%(asctime)s [%(levelname)s] %(message)s")
+    h.setFormatter(fmt)
+    h.addFilter(DuplicateGuard())
+    logger.addHandler(h)
+    return logger
 
 log = get_logger()
